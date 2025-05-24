9dfde6801f79e7f66e08f3810cfad5b739ece339
Mayank Agarwal <agarwal.mayank762@gmail.com>
2025-05-24 17:41:28 +0530

    fixing issues

--- CUSDM63S3_2521_S02/Intro-To-Redux/src/store/index.jsx
+++ CUSDM63S3_2521_S02/Intro-To-Redux/src/store/index.jsx
@@ -2,6 +2,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './counterSlice';
 
+// eslint-disable-next-line react-refresh/only-export-components
 export default configureStore({
   reducer: {
     counter: counterReducer
